b8d9abfd1b8df7261e2bec080fe1c9bf299ff3e1
silvaalexsander <alexsander.silva14@live.com>
2026-03-07 11:32:44 -0300

    Feature - Criado a estrutura backend .net com o esqueleto do UserService

---
+++ BandHub.UserService/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using BandHub.UserService.Data;
+using BandHub.UserService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BandHub.UserService.Controllers;
+
+[ApiController]
+[Route("users")]
+public class UsersController : ControllerBase
+{
+    private readonly UserDbContext _context;
+
+    public UsersController(UserDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetAll()
+    {
+        var users = await _context.Users
+            .OrderBy(x => x.CreatedAt)
+            .ToListAsync();
+
+        return Ok(users);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<User>> Create([FromBody] CreateUserRequest request)
+    {
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name,
+            Email = request.Email,
+            PasswordHash = request.Password,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return Created($"/users/{user.Id}", user);
+    }
+}
+
+public record CreateUserRequest(string Name, string Email, string Password);
---
+++ BandHub.UserService/Data/UserDbContext.cs
@@ -0,0 +1,42 @@
+﻿using BandHub.UserService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BandHub.UserService.Data;
+
+public class UserDbContext : DbContext
+{
+    public UserDbContext(DbContextOptions<UserDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("users");
+
+            entity.HasKey(x => x.Id);
+
+            entity.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            entity.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            entity.Property(x => x.PasswordHash)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            entity.Property(x => x.CreatedAt)
+                .IsRequired();
+
+            entity.HasIndex(x => x.Email)
+                .IsUnique();
+        });
+    }
+}
